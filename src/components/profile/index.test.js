c46b09f42cf9c6e1abb8f6069281fa3c50e46152
karen <karenlukitooo@gmail.com>
2020-09-17 11:36:52 +0700

    Extract Profile component to a new folder

---
+++ src/components/profile/index.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Profile } from ".App";
+
+test("renders follow button", () => {
+    render(<Profile />);
+    const linkElement = screen.getByText("Follow");
+    expect(linkElement).toBeTruthy();
+});
